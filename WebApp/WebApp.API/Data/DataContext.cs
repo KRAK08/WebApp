c20791a4c197642c35cff866ce860d7a1de07fc0
KRAK08 <ramirezalvarezkenneth08@gmail.com>
2025-07-10 11:42:14 -0600

    Add user and role entity

--- WebApp/WebApp.API/Data/DataContext.cs
+++ WebApp/WebApp.API/Data/DataContext.cs
@@ -12,6 +12,8 @@ namespace WebApp.API.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- WebApp/WebApp.API/Program.cs
+++ WebApp/WebApp.API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddRouting(r => r.LowercaseUrls = true);
 builder.Services.AddDbContext<DataContext>(d => d.UseSqlServer(builder.Configuration.GetConnectionString("dbConn")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
